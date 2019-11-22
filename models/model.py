96d260095c22a4656826864d63d4be638e05e198
Vidhisha Balachandran <vidhishanair@gmail.com>
2019-11-21 22:07:21 -0500

    Catch error in loading state dict. Can now load older incomplete encoders

--- models/model.py
+++ models/model.py
@@ -56,9 +56,13 @@ class Model(nn.Module):
 
         if args.reload_path is not None:
             state = torch.load(args.reload_path, map_location= lambda storage, location: storage)
-            self.encoder.load_state_dict(state['encoder_state_dict'])
-            self.decoder.load_state_dict(state['decoder_state_dict'], strict=False)
-            self.reduce_state.load_state_dict(state['reduce_state_dict'])
+            try:
+                self.encoder.load_state_dict(state['encoder_state_dict'])
+                self.decoder.load_state_dict(state['decoder_state_dict'], strict=False)
+                self.reduce_state.load_state_dict(state['reduce_state_dict'])
+            except RuntimeError as er:
+                print(er)
+                print("Potential mismatch in loading keys. Stop here if not expected!")
 
     def eval(self):
         if(self.args.fixed_scorer):
